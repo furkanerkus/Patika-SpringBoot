bf857f11ab83e53f5d92798048c2aec700b347d8
Furkan Erkus <erkusfrkn@gmail.com>
2022-06-13 19:51:22 +0300

    DevTools activated

---
+++ src/main/java/com/furkanerkus/utils/ConfigPrograming.java
@@ -0,0 +1,8 @@
+package com.furkanerkus.utils;
+
+public class ConfigPrograming {
+
+    // DevTools          ==>         LiveReload
+    // File ==> Settings ==> Compiler ==> Build project automatically
+    // File ==> Settings ==> Advanced Settings ==> Allow auto-make to start even...
+}
