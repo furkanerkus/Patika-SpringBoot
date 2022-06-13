42553e66f07f91c331c9ea6e27e8d0178ab15ab3
Furkan Erkus <erkusfrkn@gmail.com>
2022-06-13 19:50:56 +0300

    IntellijShorCut

---
+++ src/main/java/com/furkanerkus/utils/IntellijShortCut.java
@@ -0,0 +1,9 @@
+package com.furkanerkus.utils;
+
+public class IntellijShortCut {
+    // ctrl +alt + s        ==>     Setting
+    // alt + insert         ==>     Generator
+    // ctrl + /             ==>     Single Comment
+    // ctrl + shift + /     ==>     Multiple Comment
+
+}
